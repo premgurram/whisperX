6076e2b1303135f6ff3893b53a4d67f4f479e814
Prem Chand Gurram <prem.gurram@ril.com>
2023-12-11 19:01:20 +0530

    test

--- whisperx/asr.py
+++ whisperx/asr.py
@@ -245,17 +245,16 @@ class FasterWhisperPipeline(Pipeline):
 
     #     return {"segments": segments, "language": language}
     def transcribe(
-        self, audio: Union[str, np.ndarray], batch_size=None, num_workers=0, language=None, task=None, chunk_size=30, print_progress = False, combined_progress=False
-    ) -> TranscriptionResult:
+        self, audio: Union[str, np.ndarray], batch_size=None, num_workers=0, task=None, chunk_size=30,
+        print_progress=False, combined_progress=False
+    ):
         if isinstance(audio, str):
             audio = load_audio(audio)
 
         def data(audio, segments):
-            print(segments)
             for seg in segments:
                 f1 = int(seg['start'] * SAMPLE_RATE)
                 f2 = int(seg['end'] * SAMPLE_RATE)
-                # print(f2-f1)
                 yield {'inputs': audio[f1:f2]}
 
         vad_segments = self.vad_model({"waveform": torch.from_numpy(audio).unsqueeze(0), "sample_rate": SAMPLE_RATE})
@@ -266,7 +265,7 @@ class FasterWhisperPipeline(Pipeline):
             offset=self._vad_params["vad_offset"],
         )
 
-        segments: List[SingleSegment] = []
+        results = []
         for idx, vad_segment in enumerate(vad_segments):
             segment_audio = audio[int(vad_segment['start'] * SAMPLE_RATE):int(vad_segment['end'] * SAMPLE_RATE)]
 
@@ -277,19 +276,27 @@ class FasterWhisperPipeline(Pipeline):
                                                            True, task=task,
                                                            language=language)
 
-            for idx, out in enumerate(self.__call__(data(audio, [vad_segments]), batch_size=batch_size, num_workers=num_workers)):
-                text = out['text']
-                if batch_size in [0, 1, None]:
-                    text = text[0]
-                segments.append(
-                    {
-                        "text": text,
-                        "start": round(vad_segments[idx]['start'], 3),
-                        "end": round(vad_segments[idx]['end'], 3)
-                    }
-                )
+            transcription_result = self.__call__(data(segment_audio, [vad_segment]), batch_size=batch_size, num_workers=num_workers)
 
-        return {"segments": segments}
+            text = transcription_result['text']
+            if batch_size in [0, 1, None]:
+                text = text[0]
+
+            segment = {
+                "text": text,
+                "start": round(vad_segment['start'], 3),
+                "end": round(vad_segment['end'], 3),
+                "language": language
+            }
+            results.append(segment)
+
+            if print_progress:
+                base_progress = ((idx + 1) / len(vad_segments)) * 100
+                percent_complete = base_progress / 2 if combined_progress else base_progress
+                print(f"Progress: {percent_complete:.2f}%...")
+
+        return {"segments": results}
+
 
     def set_default_language(self,language):
         if language=='hi' or language=='en':
