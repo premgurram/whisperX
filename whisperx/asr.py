7175cf6b3fee0cf7d36a00e3c00fa8514b9863ce
Prem Chand Gurram <prem.gurram@ril.com>
2023-12-13 12:33:38 +0530

    adding local lang

--- whisperx/asr.py
+++ whisperx/asr.py
@@ -325,7 +325,7 @@ class FasterWhisperPipeline(Pipeline):
         #         break
         language_token, language_probability = results[0][0]
         language = language_token[2:-2]
-        language = self.set_default_language(language)
+        # language = self.set_default_language(language)
         print(f"Detected language: {language} ({language_probability:.2f}) in first 30s of audio...")
         return language
 
