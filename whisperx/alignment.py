378bd3565793e98ceb226fb51f4f093056ffa62a
Prem Chand Gurram <prem.gurram@ril.com>
2023-12-13 12:11:22 +0530

    rolled out other lang

--- whisperx/alignment.py
+++ whisperx/alignment.py
@@ -19,40 +19,48 @@ from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
 
 PUNKT_ABBREVIATIONS = ['dr', 'vs', 'mr', 'mrs', 'prof']
 
-LANGUAGES_WITHOUT_SPACES = ["ja", "zh"]
+# LANGUAGES_WITHOUT_SPACES = ["ja", "zh"]
 
 DEFAULT_ALIGN_MODELS_TORCH = {
     "en": "WAV2VEC2_ASR_BASE_960H",
-    "fr": "VOXPOPULI_ASR_BASE_10K_FR",
-    "de": "VOXPOPULI_ASR_BASE_10K_DE",
-    "es": "VOXPOPULI_ASR_BASE_10K_ES",
-    "it": "VOXPOPULI_ASR_BASE_10K_IT",
+    # "fr": "VOXPOPULI_ASR_BASE_10K_FR",
+    # "de": "VOXPOPULI_ASR_BASE_10K_DE",
+    # "es": "VOXPOPULI_ASR_BASE_10K_ES",
+    # "it": "VOXPOPULI_ASR_BASE_10K_IT",
 }
 
 DEFAULT_ALIGN_MODELS_HF = {
-    "ja": "jonatasgrosman/wav2vec2-large-xlsr-53-japanese",
-    "zh": "jonatasgrosman/wav2vec2-large-xlsr-53-chinese-zh-cn",
-    "nl": "jonatasgrosman/wav2vec2-large-xlsr-53-dutch",
-    "uk": "Yehor/wav2vec2-xls-r-300m-uk-with-small-lm",
-    "pt": "jonatasgrosman/wav2vec2-large-xlsr-53-portuguese",
-    "ar": "jonatasgrosman/wav2vec2-large-xlsr-53-arabic",
-    "cs": "comodoro/wav2vec2-xls-r-300m-cs-250",
-    "ru": "jonatasgrosman/wav2vec2-large-xlsr-53-russian",
-    "pl": "jonatasgrosman/wav2vec2-large-xlsr-53-polish",
-    "hu": "jonatasgrosman/wav2vec2-large-xlsr-53-hungarian",
-    "fi": "jonatasgrosman/wav2vec2-large-xlsr-53-finnish",
-    "fa": "jonatasgrosman/wav2vec2-large-xlsr-53-persian",
-    "el": "jonatasgrosman/wav2vec2-large-xlsr-53-greek",
-    "tr": "mpoyraz/wav2vec2-xls-r-300m-cv7-turkish",
-    "da": "saattrupdan/wav2vec2-xls-r-300m-ftspeech",
-    "he": "imvladikon/wav2vec2-xls-r-300m-hebrew",
-    "vi": 'nguyenvulebinh/wav2vec2-base-vi',
-    "ko": "kresnik/wav2vec2-large-xlsr-korean",
-    "ur": "kingabzpro/wav2vec2-large-xls-r-300m-Urdu",
+    # "ja": "jonatasgrosman/wav2vec2-large-xlsr-53-japanese",
+    # "zh": "jonatasgrosman/wav2vec2-large-xlsr-53-chinese-zh-cn",
+    # "nl": "jonatasgrosman/wav2vec2-large-xlsr-53-dutch",
+    # "uk": "Yehor/wav2vec2-xls-r-300m-uk-with-small-lm",
+    # "pt": "jonatasgrosman/wav2vec2-large-xlsr-53-portuguese",
+    # "ar": "jonatasgrosman/wav2vec2-large-xlsr-53-arabic",
+    # "cs": "comodoro/wav2vec2-xls-r-300m-cs-250",
+    # "ru": "jonatasgrosman/wav2vec2-large-xlsr-53-russian",
+    # "pl": "jonatasgrosman/wav2vec2-large-xlsr-53-polish",
+    # "hu": "jonatasgrosman/wav2vec2-large-xlsr-53-hungarian",
+    # "fi": "jonatasgrosman/wav2vec2-large-xlsr-53-finnish",
+    # "fa": "jonatasgrosman/wav2vec2-large-xlsr-53-persian",
+    # "el": "jonatasgrosman/wav2vec2-large-xlsr-53-greek",
+    # "tr": "mpoyraz/wav2vec2-xls-r-300m-cv7-turkish",
+    # "da": "saattrupdan/wav2vec2-xls-r-300m-ftspeech",
+    # "he": "imvladikon/wav2vec2-xls-r-300m-hebrew",
+    # "vi": 'nguyenvulebinh/wav2vec2-base-vi',
+    # "ko": "kresnik/wav2vec2-large-xlsr-korean",
+    # "ur": "kingabzpro/wav2vec2-large-xls-r-300m-Urdu",
+    # "ca": "softcatala/wav2vec2-large-xlsr-catala",
     "te": "anuragshas/wav2vec2-large-xlsr-53-telugu",
     "hi": "theainerd/Wav2Vec2-large-xlsr-hindi",
-    "ca": "softcatala/wav2vec2-large-xlsr-catala",
     "ml": "gvs/wav2vec2-large-xlsr-malayalam",
+    "bn": "tanmoyio/wav2vec2-large-xlsr-bengali",
+    "mr": "infinitejoy/wav2vec2-large-xls-r-300m-marathi-cv8",
+    "ta": "Amrrs/wav2vec2-large-xlsr-53-tamil",
+    "gj": "jaimin/wav2vec2-base-gujarati-demo",     # should work on this
+    "ka": "TheAIchemist13/kannada_beekeeping_wav2vec2",
+    "od": "theainerd/wav2vec2-large-xlsr-53-odia",
+    "pj": "gagan3012/wav2vec2-xlsr-punjabi"
+
 }
 
 
