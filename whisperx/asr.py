a0f1c1c267ec13d87381759683b756fd29f60b4e
varunnalluru <nalluru.varun@ril.com>
2024-03-26 19:52:37 +0530

    added inference for language detection

--- whisperx/asr.py
+++ whisperx/asr.py
@@ -247,10 +247,10 @@ class FasterWhisperPipeline(Pipeline):
                 print(f"Progress: {percent_complete:.2f}%...")
             # print("out",out)
             # device=  
-            model_or, align_metadata,processor_or = load_align_model('or',device="cuda",model_name)
-            model_ml, align_metadata,processor_ml = load_align_model('or',device="cuda",model_name)    
+            model_or, align_metadata,processor_or = load_align_model('or',device="cuda",model_name=None)
+            model_ml, align_metadata,processor_ml = load_align_model('or',device="cuda",model_name=None)    
             if(actual_language=='or'):
-                model_or, align_metadata,processor_or = load_align_model('or',device,model_name)
+                #model_or, align_metadata,processor_or = load_align_model('or',device,model_name)
                 inputs = processor(audio[int(round(vad_segments[idx]['start'], 3)*16000):int(round(vad_segments[idx]['end'], 3)*16000)] , sampling_rate=16_000, return_tensors="pt", padding=True)
 
                 with torch.no_grad():
@@ -261,7 +261,7 @@ class FasterWhisperPipeline(Pipeline):
 
                  
             if(actual_language=='ml'):
-                model_ml, align_metadata,processor = load_align_model('ml',device,model_name)
+                #model_ml, align_metadata,processor = load_align_model('ml',device,model_name)
                 inputs = processor_ml(audio[int(round(vad_segments[idx]['start'], 3)*16000):int(round(vad_segments[idx]['end'], 3)*16000)] , sampling_rate=16_000, return_tensors="pt", padding=True)
 
                 with torch.no_grad():
